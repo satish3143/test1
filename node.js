c0e79b47fd56d3399cad399027d11dd8ab7fad0e
unknown <satish3143>
2025-05-05 18:40:34 +0530

    added node.js file in feature branch

---
+++ node.js
@@ -0,0 +1,11 @@
+// server.js
+// const http = require('http');
+//
+// const server = http.createServer((req, res) => {
+//   res.end('Hello, this is a simple Node.js server!');
+//   });
+//
+//   server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000/');
+//     });
+//
